c4c50adc1e81515b3c84a6140473304461c5e30f
s.sepahvand <sepahvand9900@gmail.com>
2022-08-24 17:07:42 +0430

    add splash screen ui to project

--- app/src/main/java/com/samansepahvand/photoeditortest/ui/activity/SplashScreen.java
+++ app/src/main/java/com/samansepahvand/photoeditortest/ui/activity/SplashScreen.java
@@ -2,7 +2,11 @@ package com.samansepahvand.photoeditortest.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.samansepahvand.photoeditortest.R;
 
@@ -11,6 +15,24 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        handler();
+    }
+
+    private void handler(){
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },9000);
     }
 }
